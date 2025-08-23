8c30bb81126f05c0cff4c296115e9c2f488261a0
Анастасия Кор <idealizm@mail.ru>
2025-08-23 12:49:30 +0300

    3 step

--- src/app.js
+++ src/app.js
@@ -2,16 +2,31 @@ import * as yup from 'yup'
 import _ from 'lodash'
 import onChange from 'on-change'
 import initView from './view.js'
+import i18next from 'i18next'
+import resources from './locales/index';
 
 
 const duplicateUrlCheck = (list, value) => {
-const result =  _.includes(list, value)
-if(result === false) {
-    return true
-} else {return false}
+    const result = _.includes(list, value)
+    if (result === false) {
+        return true
+    } else { return false }
 };
 
 const app = () => {
+
+    const i18n = i18next.createInstance();
+    i18n.init({
+        lng: 'ru',
+        debug: true,
+        resources: {
+            ru: resources.ru
+        },
+    }).then(() => {
+        console.log('Инициализация завершена');
+    }).catch((error) => {
+        console.error('Ошибка инициализации:', error);
+    });
 
     const state = {
         form: {
@@ -22,55 +37,62 @@ const app = () => {
         },
         rssList: []
     }
+ yup.setLocale({
+    string: {
+      required: `${i18n.t('form.errors.validation.required')}`,
+      url: `${i18n.t('form.errors.validation.url')}`,
+      //unique: `${i18n.t('form.errors.validation.unique')}`
+    }
+})
 
     const schema = yup.object().shape({
-    value: yup
-    .string()
-    .url('Ссылка должна быть валидным URL')
-    .test("Unique", "Такой URL уже добавлен", (value) => {
-      return duplicateUrlCheck(state.rssList, value);
+        value: yup
+            .string()
+            .url()
+            .test("unique", `${i18n.t('form.errors.validation.unique')}`, (value) => {
+                return duplicateUrlCheck(state.rssList, value);
+            })
+            .required(),
     })
-    .required('Это обязательное поле'),
-})
 
-const validate = (fields) => {
-  return schema.validate(fields, { abortEarly: false })
-    .then(() => ({}))  
-    .catch(e => _.keyBy(e.inner, 'path'));  
-}
+    const validate = (fields) => {
+        return schema.validate(fields, { abortEarly: false })
+            .then(() => ({}))
+            .catch(e => _.keyBy(e.inner, 'path'));
+    }
 
-const elements = {
-    input: document.getElementById('url-input'),
-    submit: document.querySelector('[type="submit"]'),
-    feedback: document.querySelector('.feedback'),
-    form: document.querySelector('.rss-form')
-}
+    const elements = {
+        input: document.getElementById('url-input'),
+        submit: document.querySelector('[type="submit"]'),
+        feedback: document.querySelector('.feedback'),
+        form: document.querySelector('.rss-form')
+    }
 
-const watchState = onChange(state, initView(elements))
+    const watchState = onChange(state, initView(elements))
 
-elements.input.addEventListener('input', (e) => {
-const value = e.target.value
-watchState.form.field.value = value
+    elements.input.addEventListener('input', (e) => {
+        const value = e.target.value
+        watchState.form.field.value = value
 
-})
-
-elements.form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  watchState.form.error = {};
-
-  validate(watchState.form.field) 
-    .then((error) => {
-      watchState.form.error = error;
-      if (Object.keys(error).length === 0) {
-        state.rssList.push(watchState.form.field.value);
-        elements.input.value = '';
-      }
     })
-    .catch((error) => {
-    
-      console.error('Unexpected validation error:', error);
+
+    elements.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        watchState.form.error = {};
+
+        validate(watchState.form.field)
+            .then((error) => {
+                watchState.form.error = error;
+                if (Object.keys(error).length === 0) {
+                    state.rssList.push(watchState.form.field.value);
+                    elements.input.value = '';
+                }
+            })
+            .catch((error) => {
+
+                console.error('Unexpected validation error:', error);
+            });
     });
-});
 
 }
 export { app }
---
+++ src/locales/ru.js
@@ -0,0 +1,21 @@
+export default {
+    translation: {
+        head: {
+            h1: 'RSS агрегатор',
+            label: 'Начните читать RSS сегодня! Это легко, это красиво.'
+        },
+        form: {
+            inputLabel: 'Ссылка RSS',
+            submit: 'Добавить',
+            errors: {
+                validation: {
+                    required: 'Это обязательное поле',
+                    url: 'Ссылка должна быть валидным URL',
+                    unique: 'Такой URL уже добавлен'
+                }
+            }
+        }
+
+
+    }
+}
